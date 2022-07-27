72d170d7a2677d40d3497d37200eb99b24071aac
yoshi-non <himekawa.yoshino44@gmail.com>
2022-07-27 23:55:45 +0900

    change ページのレンダリング位置の入れ替え

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
 import type { NextPage } from 'next'
+import Sidebar from '../components/Sidebar'
 
 const Home: NextPage = () => {
   return (
-    <div>main</div>
+    <>
+      <div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh] flex-1'>
+        main
+      </div>
+      <div className='h-[92vh] overflow-hidden xl:hover:overflow-y-auto'>
+        <Sidebar/>
+      </div>
+    </>
   )
 }
 
