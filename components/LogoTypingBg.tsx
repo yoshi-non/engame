490df0488d986f78c8bd40820fe034184404046b
yoshi-non <himekawa.yoshino44@gmail.com>
2022-07-30 19:00:13 +0900

    change LogoTypingページの背景のページ遷移時エラーの解消

--- components/LogoTypingBg.tsx
+++ components/LogoTypingBg.tsx
@@ -3,9 +3,13 @@ import React, { useEffect, useRef } from 'react'
 const LogoTypingBg = () => {
   const canvasRef = useRef(null);
 
-  const getContext = (): CanvasRenderingContext2D => {
+  const getContext = (): CanvasRenderingContext2D | null => {
     const canvas: any = canvasRef.current;
-    return canvas.getContext('2d');
+    if (canvas) {
+      return canvas.getContext('2d');
+    } else {
+      return null
+    }
   }
   
   const cols = Math.floor(2400 / 20) + 1;
@@ -13,22 +17,23 @@ const LogoTypingBg = () => {
   
   useEffect(() => {
     const matrix = () => {
-      const ctx: CanvasRenderingContext2D = getContext();
-      ctx.fillStyle = '#0001';
-      ctx.fillRect(0,0, 2400, 700);
-      ctx.fillStyle = '#0f5';
-      ctx.font = '20pt Arial Monospaced MT Std';
-      console.log(ctx)
-      ypos.forEach((y, ind) => {
-        const text = String.fromCharCode(Math.random() * 128);
-        const x = ind * 20;
-        ctx.fillText(text, x, y);
-        if (y > 100 + Math.random() * 10000) ypos[ind] = 0;
-        else ypos[ind] = y + 20;
-      });
+      const ctx: CanvasRenderingContext2D | null = getContext();
+      if (ctx) {
+        ctx.fillStyle = '#0001';
+        ctx.fillRect(0,0, 2400, 700);
+        ctx.fillStyle = '#0f5';
+        ctx.font = '20pt Arial Monospaced MT Std';
+        ypos.forEach((y, ind) => {
+          const text = String.fromCharCode(Math.random() * 128);
+          const x = ind * 20;
+          ctx.fillText(text, x, y);
+          if (y > 100 + Math.random() * 10000) ypos[ind] = 0;
+          else ypos[ind] = y + 20;
+        });
+      }
     }
     setInterval(matrix, 44);
-  })
+  }, [])
 
   return (
     <canvas className="canvas z-1 absolute w-full h-full" width={700} height={600} ref={canvasRef} />
