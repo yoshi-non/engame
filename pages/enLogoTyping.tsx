7e6a36e1cd56d2242e574c9cd6b6d45c0b768fed
yoshi-non <himekawa.yoshino44@gmail.com>
2022-07-30 20:14:23 +0900

    change enLogoTypingページのスタート画面デザイン完成

--- pages/enLogoTyping.tsx
+++ pages/enLogoTyping.tsx
@@ -7,12 +7,12 @@ const enLogoTyping = () => {
     <div className='relative w-full h-[700px] overflow-hidden'>
         <LogoTypingBg/>
         <div className='absolute w-full h-[700px] text-3xl font-medium z-3 flex flex-col justify-center items-center'>
-            <p className='text-white mb-[100px]'>Engineer Logo Typing</p>
+            <p className='enLogoTitle text-white mb-[100px]'>Engineer Logo Typing</p>
             <div className='text-center mb-10'>
-                <div className='bg-primary rounded-full px-7 py-3 mb-5'>遊び方</div>
-                <div className='bg-primary rounded-full px-7 py-3 mb-5'>設定</div>
+                <div className='cursor-pointer bg-black shadow-[#0f5] tracking-widest shadow text-white bg-opacity-75 rounded-full px-7 py-3 mb-10'>遊び方</div>
+                <div className='cursor-pointer bg-black shadow-[#0f5] tracking-widest shadow text-white bg-opacity-75 rounded-full px-7 py-3 mb-10'>設定</div>
             </div>
-            <div className='text-white'>Spaceキーでスタート</div>
+            <div className='enLogoStart text-white tracking-wide'>Spaceキーでスタート</div>
         </div>
     </div>
   )
