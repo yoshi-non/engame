a79e94727df8bb32c45b4f2a033c0d48da10cf08
yoshi-non <himekawa.yoshino44@gmail.com>
2022-08-07 20:45:31 +0900

    add FourChoice 結果画面以外のアルゴリズム完成

--- pages/FourChoice.tsx
+++ pages/FourChoice.tsx
@@ -3,41 +3,78 @@ import fourChoiceData from '../fourChoiceData';
 import Modal from 'react-modal'
 
 const FourChoice = () => {
+
   const [secs, setSeconds] = useState(0);
-  const [gameTime, setGameTime] = useState<number | null>(null)
+  // ライフ
+  const [gameLife, setGameLife] = useState(3);
+  // 現在出力している問題のカウント
+  const [dataCount, setDataCount] = useState(0)
+
   useEffect(() => {
     let sampleInterval = setInterval(() => {
       if (secs > 0) {
         setSeconds(secs - 1)
       }
-      if (gameTime) {
-        setGameTime(gameTime - 1)
-      }
       if (secs === 0) {
         clearInterval(sampleInterval)
       }
     }, 1000);
-    if (gameTime === 0) {
-      setResult(true)
-      setGameTime(null)
+    // 問題数が3問正解したら終了
+    if (dataCount + 1 > 3) {
+      setGameOn(false)
+      setDataCount(0)
+    }
+    // ライフが0になったら終了
+    if (gameLife == 0) {
+      setGameOn(false)
       setDataCount(0)
     }
     return () => {
       clearInterval(sampleInterval)
     }
   })
-
+  // ゲームが行えるかどうかの判定
+  const [gameOn, setGameOn] = useState(false);
   // 結果画面を表示させるかの判定
-  const [result, setResult] = useState(false)
+  // const [result, setResult] = useState(false)
   // 正解している問題数
   const [correctCount, setCorrectCount] = useState(0)
-  // 現在出力している問題のカウント
-  const [dataCount, setDataCount] = useState(0)
   // 問題情報
   const data = fourChoiceData
-
+  // 現在出力している問題情報
+  const questionData = data.map(item => item["question"])
+  let currentQuestion = questionData[dataCount]
+  const choiceDataA = data.map(item => item["choiceA"])
+  let currentChoiceA = choiceDataA[dataCount]
+  const choiceDataB = data.map(item => item["choiceB"])
+  let currentChoiceB = choiceDataB[dataCount]
+  const choiceDataC = data.map(item => item["choiceC"])
+  let currentChoiceC = choiceDataC[dataCount]
+  const choiceDataD = data.map(item => item["choiceD"])
+  let currentChoiceD = choiceDataD[dataCount]
+  const answerData = data.map(item => item["answer"])
+  let currentAnswer = answerData[dataCount]
   // ゲームスタート
-
+  const gameStart = () => {
+    setGameOn(true)
+    setGameLife(3)
+    setSeconds(3)
+    setCorrectCount(0)
+  }
+  
+  // 問題に答えた時の処理
+  const answerCheck: any = (userChoice:any) => {
+    if (userChoice == currentAnswer) {
+      // 問題に正解したとき
+      setDataCount(dataCount +1)
+      setCorrectCount(correctCount + 1)
+    } else {
+      // 問題に間違えたとき
+      setDataCount(dataCount +1)
+      setGameLife(gameLife - 1)
+    }
+    
+  }
 
   // modal
   // モーダルを画面中央に表示する用のスタイル
@@ -62,64 +99,75 @@ const FourChoice = () => {
     setIsOpen(false)
   }
 
-
   return (
     <div className='relative w-full h-[700px] overflow-hidden bg-[#f5e4b7]'>
 
       {/* カウントダウン表示 */}
-      {/* <div className='absolute w-full h-[700px] text-[6rem] font-medium text-white z-3 flex flex-col justify-center items-center'>
+      {(secs > 0) && (
+      <div className='absolute w-full h-[700px] text-[6rem] font-medium z-3 flex flex-col justify-center items-center'>
         <p>{secs}</p>
-      </div> */}
-      
+      </div>
+      )}
       {/* 問題表示画面 */}
-      {/* <div className='absolute w-full h-[700px] text-[2.5rem] font-medium text-black z-3 flex flex-col justify-center items-center gap-5'>
-        <div className='flex justify-between items-center w-[80%]'>
-          <p>01</p>
-          <p>♥ ♥ ♥</p>
+      {(secs == 0 && gameOn) && (
+        <div className='absolute w-full h-[700px] text-[2.5rem] font-medium text-black z-3 flex flex-col justify-center items-center gap-5'>
+          <div className='flex justify-between items-center w-[80%]'>
+            <p>{dataCount + 1}/5</p>
+            <p className='flex'>
+              {"♥".repeat(gameLife)}
+            </p>
+          </div>
+          <p className='border-[4px] border-black text-center py-5 w-[80%]'>{currentQuestion}</p>
+          <p onClick={() => answerCheck(currentChoiceA)} className='border-[4px] border-black text-center w-[80%] cursor-pointer'><span>①</span>{currentChoiceA}</p>
+          <p onClick={() => answerCheck(currentChoiceB)} className='border-[4px] border-black text-center w-[80%] cursor-pointer'><span>②</span>{currentChoiceB}</p>
+          <p onClick={() => answerCheck(currentChoiceC)} className='border-[4px] border-black text-center w-[80%] cursor-pointer'><span>③</span>{currentChoiceC}</p>
+          <p onClick={() => answerCheck(currentChoiceD)} className='border-[4px] border-black text-center w-[80%] cursor-pointer'><span>④</span>{currentChoiceD}</p>
         </div>
-        <p className='border-[4px] border-black text-center py-5 w-[80%]'>{data[0].question}</p>
-        <p className='border-[4px] border-black text-center w-[80%] cursor-pointer'><span>①</span>{data[0].choiceA}</p>
-        <p className='border-[4px] border-black text-center w-[80%] cursor-pointer'><span>②</span>{data[0].choiceB}</p>
-        <p className='border-[4px] border-black text-center w-[80%] cursor-pointer'><span>③</span>{data[0].choiceC}</p>
-        <p className='border-[4px] border-black text-center w-[80%] cursor-pointer'><span>④</span>{data[0].choiceD}</p>
-        <div>10</div>
-      </div> */}
+      )}
 
       {/* ゲーム終了時結果画面 */}
       {/* 後で記載 */}
 
       {/* ホーム画面(ゲームが始まっていない) */}
-
-      <div className='absolute w-full h-[700px] text-[2.5rem] font-medium z-3 flex flex-col justify-center items-center gap-4'>
-        <p className='text-[4rem] font-bold'>エンジニア常識クイズ</p>
-        <div
-          className='border-[4px] border-black text-center w-[80%] cursor-pointer'
-          onClick={openModal}
-        >
-            遊び方
-        </div>
-        <div className='border-[4px] border-black text-center w-[80%] cursor-pointer'>START</div>
-          <Modal
-            isOpen={modalIsOpen}
-            style={customStyles}
-            onRequestClose={closeModal}
-            ariaHideApp={false}
+      {(secs == 0 && !gameOn) && (
+        <div className='absolute w-full h-[700px] text-[2.5rem] font-medium z-3 flex flex-col justify-center items-center gap-4'>
+          <p className='text-[4rem] font-bold'>エンジニア常識クイズ</p>
+          <div
+            className='border-[4px] border-black text-center w-[80%] cursor-pointer'
+            onClick={openModal}
           >
-            <div className='w-[45vw] text-xl text-gray-500 font-[500] p-10'>
-              <div className='flex flex-col gap-10 justify-center items-center'>
-                <p>エンジニアの常識クイズです。</p>
-                <p className='text-center text-black bg-[#f5e4b7] px-7 py-3 rounded'>
-                  ※現在はテスト中のため5問になります。
-                </p>
-                <p>
-                  ゲーム中は「ESCキー」でタイトルに戻ります。<br />
-                  「STARTボタン」を押すと始められます。
-                </p>
-                <button onClick={closeModal} className="text-white bg-[#3f403f73] px-5 py-3 rounded-full hover:opacity-70">閉じる</button>
+            遊び方
+          </div>
+          <div
+            className='border-[4px] border-black text-center w-[80%] cursor-pointer'
+            onClick={gameStart}
+          >
+            START
+          </div>
+            <Modal
+              isOpen={modalIsOpen}
+              style={customStyles}
+              onRequestClose={closeModal}
+              ariaHideApp={false}
+            >
+              <div className='w-[45vw] text-xl text-gray-500 font-[500] p-10'>
+                <div className='flex flex-col gap-10 justify-center items-center'>
+                  <p>エンジニアの常識クイズです。</p>
+                  <p className='text-center text-black bg-[#f5e4b7] px-7 py-3 rounded'>
+                    ※現在はテスト中のため5問になります。
+                  </p>
+                  <p>※ライフは3つありクイズで間違えるとライフが1つ減り0になるとゲームが終了します。</p>
+                  <p>
+                    ゲーム中は「ESCキー」でタイトルに戻ります。<br />
+                    「STARTボタン」を押すと始められます。
+                  </p>
+                  <button onClick={closeModal} className="text-white bg-[#3f403f73] px-5 py-3 rounded-full hover:opacity-70">閉じる</button>
+                </div>
               </div>
-            </div>
-          </Modal>
-      </div>
+            </Modal>
+        </div>
+      )}
+
     </div>
   )
 }
